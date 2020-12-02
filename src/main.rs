25ea674436b10c345d1a8aa674ed96afd0694783
Paul Gottschaldt <paul.gottschaldt1997@freenet.de>
2020-12-02 11:54:12 +0100

    first task solved

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,43 @@
-use std::env;
 use std::fs;
 
 fn main() {
     // --snip--
-    let filename = "src/First/input.txt";
+    let filename = "src/Second/input.txt";
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-
+    let strings: Vec<&str> = contents.lines().collect();
     //println!("With text:\n{}", contents);
-    let mut split: Vec<i32> = contents.lines().map(|s| s.parse().unwrap()).collect();
+    //let mut split: Vec<(&str, i32)> = contents
+    let mut correct_values: i32 = 0;
+    for x in strings {
+        let test_string_splitted: Vec<&str> = x.split(": ").collect();
+        let test_string = test_string_splitted[1];
+        let char_string_splitted: Vec<&str> = test_string_splitted[0].split_whitespace().collect();
+        let char_value = char_string_splitted[1];
+        let min_max_splitted: Vec<usize> = char_string_splitted[0]
+            .split("-")
+            .map(|s| s.parse().unwrap())
+            .collect();
 
-    for num1 in &split {
-        //println!("{}", num);
-        if num1 < &1001 {
-            println!("{} is smaller than 1001", num1);
-            for num2 in &split {
-                if num2 < &1001 {
-                    for num3 in &split {
-                        if num2 + num1 + num3 == 2020 {
-                            println!(
-                                "{}, {} and {} sum up to 2020, product is {}",
-                                num1,
-                                num2,
-                                num3,
-                                num1 * num2 * num3
-                            );
-                        }
-                    }
-                }
-            }
+        // println!(
+        //     "search for min: {} or max: {} of: {} in string {}",
+        //     min_max_splitted[0], min_max_splitted[1], char_value, test_string
+        // );
+        let counted_occurences = test_string.matches(char_value).count();
+        if counted_occurences <= min_max_splitted[1] && counted_occurences >= min_max_splitted[0] {
+            correct_values += 1;
+            println!(
+                "search for min: {} or max: {} of: {} in string {}, was found: {} : Correct",
+                min_max_splitted[0],
+                min_max_splitted[1],
+                char_value,
+                test_string,
+                counted_occurences
+            );
         }
     }
+    println!("Found {} correct entries", correct_values);
+    //let mut splitted: Vec<(&str)> = strings.split(" : ");
+    //.map(|s| s.parse().unwrap())
 }
