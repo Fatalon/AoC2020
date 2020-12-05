86c4afeb6f9ea2658f1ff057bd32a65388bc1d22
Paul Gottschaldt <paul.gottschaldt1997@freenet.de>
2020-12-05 11:10:00 +0100

    Solved day5

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ fn main() {
 
     let mut highest_seat_id: isize = 0;
 
+    let mut values_per_row: Vec<isize> = vec![0; 128];
+
     for seat in seats {
         println!("Seat is {}", seat);
         let row = &seat.replace("B", "1").replace("F", "0")[..7];
@@ -23,8 +25,23 @@ fn main() {
         println!("col is: {}", col);
 
         let seat_id = row * 8 + col;
+        let mut current_summed_cols = values_per_row[row as usize];
+        current_summed_cols += col + 1;
+        values_per_row[row as usize] = current_summed_cols;
         if seat_id > highest_seat_id {
             highest_seat_id = seat_id;
+        }
+    }
+    let expected_sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+    for (i, summed_col) in values_per_row.iter().enumerate() {
+        if summed_col < &expected_sum {
+            let missing_col = expected_sum - summed_col - 1;
+            println!(
+                "the missing seat is in row {} at col {} with index {}",
+                i,
+                missing_col,
+                i * 8 + missing_col as usize
+            );
         }
     }
     println!("highest seat id was: {}", highest_seat_id);
