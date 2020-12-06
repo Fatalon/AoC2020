1fbbcd06ea6f55a71245446c77aae9944ce36243
Paul Gottschaldt <paul.gottschaldt1997@freenet.de>
2020-12-06 10:59:20 +0100

    Solved part 1

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,32 @@
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     // --snip--
-    let filename = "src/Day5/input.txt";
+    let filename = "src/Day6/input.txt";
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let seats: Vec<&str> = contents.lines().collect();
+    let groups: Vec<&str> = contents.split("\n\n").collect();
 
-    let mut highest_seat_id: isize = 0;
+    let mut total_score: usize = 0;
 
-    let mut values_per_row: Vec<isize> = vec![0; 128];
-
-    for seat in seats {
-        println!("Seat is {}", seat);
-        let row = &seat.replace("B", "1").replace("F", "0")[..7];
-        println!("row in binary is: {}", row);
-        let row = isize::from_str_radix(row, 2).unwrap();
-        println!("row is: {}", row);
-
-        let col = &seat.replace("L", "0").replace("R", "1")[7..10];
-        println!("col in binary is: {}", col);
-        let col = isize::from_str_radix(col, 2).unwrap();
-        println!("col is: {}", col);
-
-        let seat_id = row * 8 + col;
-        let mut current_summed_cols = values_per_row[row as usize];
-        current_summed_cols += col + 1;
-        values_per_row[row as usize] = current_summed_cols;
-        if seat_id > highest_seat_id {
-            highest_seat_id = seat_id;
+    for group in groups {
+        let mut total_answers = HashSet::new();
+        let answers: Vec<&str> = group.lines().collect();
+        for answer in answers {
+            for c in answer.chars() {
+                total_answers.insert(c);
+                // do something with `c`
+            }
+            // do something with `c`
         }
+        println!(
+            "Found {} disjunct answers in {}",
+            total_answers.len(),
+            group.replace("\n", "\t")
+        );
+        total_score += total_answers.len();
     }
-    let expected_sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
-    for (i, summed_col) in values_per_row.iter().enumerate() {
-        if summed_col < &expected_sum {
-            let missing_col = expected_sum - summed_col - 1;
-            println!(
-                "the missing seat is in row {} at col {} with index {}",
-                i,
-                missing_col,
-                i * 8 + missing_col as usize
-            );
-        }
-    }
-    println!("highest seat id was: {}", highest_seat_id);
+    println!("total score was: {}", total_score);
 }
