d04405277137af0a8cd1e5fb5d0459399bfa5f6b
Paul Gottschaldt <paul.gottschaldt1997@freenet.de>
2020-12-07 21:35:26 +0100

    Solved part 1

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,43 @@ use std::collections::HashSet;
 use std::fs;
 
 fn main() {
-    // --snip--
-    let filename = "src/Day6/input.txt";
+    // --snip
+    let filename = "src/Day7/input.txt";
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let groups: Vec<&str> = contents.split("\n\n").collect();
+    let mut rules: Vec<&str> = contents.split(".\n").collect();
 
-    let mut total_score: usize = 0;
+    let mut found = HashSet::new();
+    found.insert("shiny gold");
+    let mut total_options: usize = 0;
 
-    for group in groups {
-        let mut total_answers = HashSet::new();
-        let mut current_answers = HashSet::new();
-        let answers: Vec<&str> = group.lines().collect();
-        let first_answer = answers[0];
-
-        //init
-        for c in first_answer.chars() {
-            total_answers.insert(c);
-        }
-
-        //intesect
-        for answer in answers {
-            current_answers.clear();
-            for c in answer.chars() {
-                current_answers.insert(c);
+    while total_options != found.len() {
+        total_options = found.len();
+        for rule in &rules {
+            let items: Vec<&str> = rule.split("s contain ").collect();
+            if rule.len() == 0 {
+                println!("found empty last rule");
+                continue;
             }
-            total_answers = total_answers
-                .iter()
-                .filter(|k| current_answers.contains(k))
-                .map(|&x| x)
-                .collect::<HashSet<char>>();
-            //total_answers.intersection(&current_answers);
+            println!("rule was {}", rule);
+            println!("rule for bag {} was {}", items[0], items[1]);
+
+            if items[1].contains("no other bags") {
+                println!("found special case");
+                continue;
+            }
+            let contained_items: Vec<&str> = items[1].split(", ").collect();
+
+            for contained_item in contained_items {
+                let amounts: Vec<&str> = contained_item.split(" ").collect();
+                let amount: i32 = amounts[0].parse().unwrap();
+                if found.iter().any(|&i| contained_item.contains(i)) {
+                    println!("Found one already present entry, push new one in");
+                    found.insert(items[0]);
+                }
+            }
         }
-        println!(
-            "Found {} same answers in {}",
-            total_answers.len(),
-            group.replace("\n", "\t")
-        );
-        total_score += total_answers.len();
     }
-    println!("total score was: {}", total_score);
+    println!("total options for shiny gold was: {}", found.len() - 1);
 }
