48e31a390378799724f249ed8bd88c2850b7bc3a
Paul Gottschaldt <paul.gottschaldt1997@freenet.de>
2020-12-13 13:08:28 +0100

    Solved part 1

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,88 @@
 use std::fs;
-use std::iter::FromIterator;
 
-fn performActions(instructions: Vec<(char, i32)>) -> (i32, i32) {
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
-
-    let mut waypoint_x: i32 = 10;
-    let mut waypoint_y: i32 = 1;
-    //directions are 0 - east, 1 - north, 2 - west, 3 - south
-
-    for instruction in instructions {
-        if instruction.0 == 'N' {
-            waypoint_y += instruction.1;
-        } else if instruction.0 == 'S' {
-            waypoint_y -= instruction.1;
-        } else if instruction.0 == 'E' {
-            waypoint_x += instruction.1;
-        } else if instruction.0 == 'W' {
-            waypoint_x -= instruction.1;
-        } else if instruction.0 == 'R' {
-            //Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
-            let mut cos_value = 0;
-            let mut sin_value = 0;
-            match instruction.1 / 90 {
-                0 => {
-                    cos_value = 1;
-                    sin_value = 0
-                }
-                1 => {
-                    sin_value = 1;
-                    cos_value = 0;
-                }
-                2 => {
-                    sin_value = 0;
-                    cos_value = -1;
-                }
-                3 => {
-                    sin_value = -1;
-                    cos_value = 0;
-                }
-                _ => println!("something went wrong"),
-            }
-            println!("waypoint was at {}, {}", waypoint_x, waypoint_y);
-            let old_waypoint_x = waypoint_x;
-            waypoint_x = cos_value * waypoint_x + sin_value * waypoint_y;
-            waypoint_y = -sin_value * old_waypoint_x + cos_value * waypoint_y;
-
-            println!("waypoint is now at {}, {}", waypoint_x, waypoint_y);
-        } else if instruction.0 == 'L' {
-            let mut cos_value = 0;
-            let mut sin_value = 0;
-            match instruction.1 / 90 {
-                0 => {
-                    cos_value = 1;
-                    sin_value = 0
-                }
-                1 => {
-                    sin_value = 1;
-                    cos_value = 0;
-                }
-                2 => {
-                    sin_value = 0;
-                    cos_value = -1;
-                }
-                3 => {
-                    sin_value = -1;
-                    cos_value = 0;
-                }
-                _ => println!("something went wrong"),
-            }
-            println!("waypoint was at {}, {}", waypoint_x, waypoint_y);
-            let old_waypoint_x = waypoint_x;
-            waypoint_x = cos_value * waypoint_x - sin_value * waypoint_y;
-            waypoint_y = sin_value * old_waypoint_x + cos_value * waypoint_y;
-
-            println!(
-                "waypoint is now at {}, {}, with cos {} and sin {}",
-                waypoint_x, waypoint_y, cos_value, sin_value
-            );
-        } else if instruction.0 == 'F' {
-            x += instruction.1 * waypoint_x;
-            y += instruction.1 * waypoint_y;
-
-            println!(
-                "ship is now at {},{}, waypoint was at {},{}, with scalar {}",
-                x, y, waypoint_x, waypoint_y, instruction.1
-            );
+fn estimateNearest(buses: Vec<i32>, time_of_arrival: i32) -> i32 {
+    let mut nearest_value = 10000;
+    let mut nearest_bus_id = 0;
+    for bus in buses {
+        if time_of_arrival % bus == 0 {
+            nearest_value = 0;
+            nearest_bus_id = 0;
+            return 0;
+        }
+        let amount_of_departs = time_of_arrival / bus;
+        let next_depart = (amount_of_departs + 1) * bus;
+        if (next_depart - time_of_arrival) < nearest_value {
+            nearest_bus_id = bus;
+            nearest_value = next_depart - time_of_arrival;
         }
     }
-    return (x, y);
+    return nearest_value * nearest_bus_id;
+}
+
+fn estimateOptimalTimeForArrival(buses_with_offset: Vec<(i32, i32)>) {
+    let mut time: i128 = 0;
+    let mut all_valid = true;
+    for _ in 0..10000000000000000 as i128 {
+        time += buses_with_offset[0].1 as i128;
+        all_valid = true;
+        if time % 500000000 == 0 {
+            println!("time is now {}", time);
+        }
+        for entry in buses_with_offset.clone() {
+            if (time + entry.0 as i128) % entry.1 as i128 != 0 {
+                // println!(
+                //     "entry failed for time {} with values {},{}",
+                //     time, entry.0, entry.1
+                // );
+                all_valid = false;
+                break;
+            }
+        }
+        if all_valid {
+            println!("found solution at time {}", time);
+            return;
+        }
+    }
 }
 
 fn main() {
     // --snip
-    let filename = "src/Day12/input.txt";
+    let filename = "src/Day13/input.txt";
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let lines: Vec<&str> = contents.lines().collect();
 
-    let mut instructions: Vec<(char, i32)> = Vec::new();
+    let time_of_arrival = lines[0].parse().unwrap();
 
-    for line in lines {
-        let char_vec: Vec<char> = line.chars().collect();
-        let value = String::from_iter(&char_vec[1..]).parse().unwrap();
-        instructions.push((char_vec[0], value));
+    let mut buses: Vec<i32> = Vec::new();
+    for entry in lines[1].split(",") {
+        if entry == "x" {
+            continue;
+        }
+        buses.push(entry.parse().unwrap());
     }
-    let result = performActions(instructions.clone());
-    println!("manhatten distance is {}", result.0.abs() + result.1.abs());
+
+    for bus in buses.clone() {
+        print!("found {}, ", bus);
+    }
+    println!("Search for time {}", time_of_arrival);
+
+    let result = estimateNearest(buses.clone(), time_of_arrival);
+    println!("result is {}", result);
+
+    let mut buses_with_offset: Vec<(usize, usize)> = Vec::new();
+
+    let buses = lines[1]
+        .split(',')
+        .enumerate()
+        .filter(|&(_, bus_id)| bus_id != "x")
+        .map(|(i, bus_id)| (i, bus_id.parse::<usize>().unwrap()))
+        .collect::<Vec<_>>();
+
+    for bus in buses.clone() {
+        println!("bus with id {} should depart at {}", bus.1, bus.0);
+    }
+
+    //estimateOptimalTimeForArrival(buses_with_offset.clone());
 }
