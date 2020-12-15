762b2b3590ade7b09be6c6080330da0302b530e0
Paul Gottschaldt <paul.gottschaldt1997@freenet.de>
2020-12-15 21:17:04 +0100

    Solved part 1

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,73 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs;
-
-fn generatePossibleAddresses(
-    mut floating_indices: Vec<usize>,
-    bin_address: Vec<char>,
-) -> Vec<Vec<char>> {
-    if floating_indices.len() == 0 {
-        return vec![bin_address];
-    }
-    let index = floating_indices.pop().unwrap();
-    let mut result = generatePossibleAddresses(floating_indices, bin_address);
-    let copy = result.clone();
-    for mut entry in copy {
-        entry[index as usize] = '1';
-        result.push(entry.clone());
-        entry[index as usize] = '0';
-        result.push(entry.clone());
-    }
-    return result;
-}
 
 fn main() {
     // --snip
-    let filename = "src/Day14/input.txt";
+    let filename = "src/Day15/input.txt";
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let lines: Vec<&str> = contents.lines().collect();
+    let values: Vec<i32> = contents.split(",").map(|s| s.parse().unwrap()).collect();
 
-    let mut memory = HashMap::new();
+    let mut game: Vec<(i32, usize)> = Vec::new();
 
-    let mut mask = "";
+    for (i, value) in values.iter().enumerate() {
+        game.push((*value, i + 1));
+    }
+    println!("game has {} entries", game.len());
 
-    for line in lines {
-        let instruction: Vec<&str> = line.split(" = ").collect();
-        if instruction[0] == "mask" {
-            mask = instruction[1];
-            println!("Mask is now \t\t  {}", mask);
-        } else {
-            let raw_int_address: i64 = instruction[0]
-                .replace("mem[", "")
-                .replace("]", "")
-                .parse()
-                .unwrap();
-            let raw_int_value: i64 = instruction[1].parse().unwrap();
+    let mut last_value = -1;
+    for index in values.len()..30000000 {
+        if index % 10000 == 0 {
+            println!("current index {}", index);
+        }
+        //println!("last value is {}", last_value);
+        //println!("Current game state:");
+        // for entry in game.clone() {
+        //     println!("{} {}", entry.0, entry.1);
+        // }
+        if last_value == -1 {
+            game.push((0, index + 1));
+            last_value = 0;
+            //println!("First iteration, index is currently {}", index);
+            continue;
+        }
+        let mut game_to_check: Vec<(i32, usize)> = game.clone()[0..game.len() - 1].to_vec();
+        game_to_check.reverse();
+        //println!("game to check has length {}", game_to_check.len());
+        // for entry in game_to_check.clone() {
+        //     println!("{} {}", entry.0, entry.1);
+        // }
 
-            println!("instruction put {} in {}", raw_int_value, raw_int_address);
-
-            let raw_bin_address = format!("{:#038b}", raw_int_address);
-            println!("bin value is \t\t{}", raw_bin_address);
-            let mut bin_address: Vec<_> = raw_bin_address.chars().collect();
-            bin_address.reverse();
-            let mut mask_vec: Vec<_> = mask.chars().collect();
-            mask_vec.reverse();
-
-            let mut floating_indices = Vec::new();
-            for n in 0..mask_vec.len() {
-                let c = mask_vec[n];
-                if c == '1' {
-                    bin_address[n] = c;
-                }
-                if c == 'X' {
-                    floating_indices.push(n);
-                }
-            }
-            let variations = i64::pow(2, floating_indices.len() as u32);
-            println!("need {} different addresses", variations);
-
-            let mut result = generatePossibleAddresses(floating_indices, bin_address);
-
-            let set: HashSet<_> = result.drain(..).collect(); // dedup
-            result.extend(set.into_iter());
-            println!("result had {} entries", result.len());
-
-            for mut entry in result {
-                entry.reverse();
-                let bin_idx: String = entry.iter().collect();
-                let bin_idx = &bin_idx[2..];
-                println!("bin is now \t\t  {}", bin_idx);
-                let int_address = i64::from_str_radix(&*bin_idx, 2).unwrap();
-                println!("insert {} into address {}", raw_int_value, int_address);
-                memory.insert(int_address, raw_int_value);
+        let mut last_time_seen = 0;
+        for (i, entry) in game_to_check.iter().enumerate() {
+            if entry.0 == last_value {
+                // println!("found last value in entry {}, {}", entry.0, entry.1);
+                last_time_seen = entry.1;
+                //println!("entry 1 is {}", entry.1);
+                game.remove(
+                    game.iter()
+                        .position(|x| x.1 == entry.1)
+                        .expect("needle not found"),
+                );
+                //game.remove(index - (i + 2));
+                break;
             }
         }
+        // value was not present before
+        if last_time_seen == 0 {
+            game.push((0, index + 1));
+            last_value = 0;
+            //println!("last value was first time inserted {}", index);
+            continue;
+        }
+        last_value = (index - last_time_seen) as i32;
+        game.push((last_value, index + 1));
+        //println!("last value was seen at time {} before", last_time_seen);
     }
-    let mut sum = 0;
-    for entry in memory {
-        sum += entry.1;
+
+    for entry in game {
+        println!("{} {}", entry.0, entry.1);
     }
-    println!("total sum is {}", sum);
+
+    println!("Solution is {}", last_value);
 }
